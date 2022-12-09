8447499b244d1418eb7aad960c51ecda641fb0f3
manamana32321 <manamana32321@gmail.com>
2022-12-09 18:11:51 +0900

    Done!

---
+++ index.js
@@ -0,0 +1,82 @@
+const PORT = process.env.PORT || 8000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express(); // initialize express
+
+const newspapers = [
+  {
+    name: "thetimes",
+    address: "https://www.thetimes.co.uk/environment/climate-change",
+    base: "",
+  },
+  {
+    name: "guardian",
+    address: "https://www.theguardian.com/environment/climate-crisis",
+    base: "",
+  },
+  {
+    name: "telegraph",
+    address: "https://www.telegraph.co.uk/climate-change",
+    base: "https://www.telegraph.co.uk",
+  },
+];
+
+const articles = [];
+
+newspapers.forEach((newspaper) => {
+  axios.get(newspaper.address).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    $('a:contains("climate")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr("href");
+      articles.push({
+        title,
+        url: newspaper.base + url,
+        source: newspaper.name,
+      });
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.json("Welcome to my Climate Change API");
+});
+
+app.get("/news", (req, res) => {
+  res.json(articles);
+});
+
+app.get("/news/:newspaperId", async (req, res) => {
+  const newspaperId = req.params.newspaperId;
+
+  const newpaperAddress = newspapers.filter(
+    (newspaper) => newspaper.name === newspaperId
+  )[0].address;
+  const newspaperbase = newspapers.filter(
+    (newpaper) => newpaper.name === newspaperId
+  )[0].base;
+
+  axios
+    .get(newpaperAddress)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const specificArticles = [];
+
+      $('a:contains("climate")', html).each(function () {
+        specificArticles.push({
+          title: $(this).text(),
+          url: newspaperbase + $(this).attr("href"),
+          source: newspaperId,
+        });
+      });
+      res.json(specificArticles);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
